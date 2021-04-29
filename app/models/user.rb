2df5d83e5e5ee2d8b29b2e747a3a32b4f8c80eeb
EC2 Default User <ec2-user@ip-172-31-95-76.ec2.internal>
2021-04-29 06:17:38 +0000

    [Add]名前ログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:name]
+         
+  validates_uniqueness_of :name
+  validates_presence_of :name
          
   has_many :books, dependent: :destroy
-  attachment :profile_image
   
 end
