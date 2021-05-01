4514cfa18f601e3b114d31bf348356df51c943f7
Miharu-380 <38t.ajlkk@gmail.com>
2021-05-01 07:07:17 +0000

    [Add]book/edit作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,10 +15,20 @@ class BooksController < ApplicationController
       render 'index'
     end
   end
-
+  
   def show
     @book = Book.find(params[:id])
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
