ab0285bf7b1ff8cef69887d8728fa6ca29435da6
Miharu-380 <38t.ajlkk@gmail.com>
2021-05-06 05:58:23 +0000

    [Add]login認証&books/indexページ編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fafe3ffe39db1cb52e4d268673657f08e88a326339e3eea837b32fe38a016ff35a82e58842b65c34f063e23c1f13c1de7c3d42dcffc251870a8010d1cefa3500'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users,controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
   
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :books
-  resources :users
+  resources :users, only: [:create, :index, :show, :edit, :update]
+  
 end
